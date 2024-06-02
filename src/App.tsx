ed258d439cc563ff8e14483c94d2d3868ebaa385
Ilya Kruchinin <kr4chinin@mail.ru>
2024-06-02 18:15:34 +0300

    format: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 const Root = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 `;
 
 const App = () => {
-	return <Root>123</Root>;
+  return <Root>123</Root>;
 };
 
 export { App };
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // @ts-ignore
 // Waiting for the patch to resolve type issue
@@ -8,34 +8,34 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react({
-			// For styled components transform to readable classnames
-			babel: {
-				presets: ['@babel/preset-typescript'],
-				plugins: [
-					'@babel/plugin-transform-typescript',
-					'@babel/plugin-proposal-class-properties',
-					[
-						// https://github.com/styled-components/babel-plugin-styled-components
-						'babel-plugin-styled-components',
-						{
-							ssr: false,
-							pure: true,
-							fileName: true,
-							displayName: true
-						}
-					]
-				]
-			}
-		}),
-		// https://github.com/gxmari007/vite-plugin-eslint
-		eslint({
-			exclude: ['/virtual:/**', 'node_modules/**']
-		})
-	],
-	server: {
-		port: 3000,
-		host: '0.0.0.0'
-	}
+  plugins: [
+    react({
+      // For styled components transform to readable classnames
+      babel: {
+        presets: ['@babel/preset-typescript'],
+        plugins: [
+          '@babel/plugin-transform-typescript',
+          '@babel/plugin-proposal-class-properties',
+          [
+            // https://github.com/styled-components/babel-plugin-styled-components
+            'babel-plugin-styled-components',
+            {
+              ssr: false,
+              pure: true,
+              fileName: true,
+              displayName: true,
+            },
+          ],
+        ],
+      },
+    }),
+    // https://github.com/gxmari007/vite-plugin-eslint
+    eslint({
+      exclude: ['/virtual:/**', 'node_modules/**'],
+    }),
+  ],
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
 });
