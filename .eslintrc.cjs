767bec5cbfff780fca242a9a8ea9f64790df8fc8
Ilya Kruchinin <kr4chinin@mail.ru>
2024-06-02 18:06:39 +0300

    docs: eslint configuration comments

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,27 @@
 module.exports = {
+	// Specifies the base configurations to extend
 	extends: [
+		// Extends the default configuration for React apps
 		'react-app',
+		// Extends recommended rules for TanStack Query
+		// https://tanstack.com/query/v4/docs/eslint/eslint-plugin-query
 		'plugin:@tanstack/eslint-plugin-query/recommended',
+		// Extends recommended rules for working with regular expressions
+		// https://github.com/ota-meshi/eslint-plugin-regexp
 		'plugin:regexp/recommended'
 	],
-	plugins: ['@tanstack/query', '@stylistic', 'regexp'],
+	plugins: [
+		// Plugin for TanStack Query
+		'@tanstack/query',
+		// Plugin for stylistic rules
+		// https://www.npmjs.com/package/@stylistic/eslint-plugin
+		'@stylistic',
+		// Plugin for regex rules
+		'regexp'
+	],
 	parser: '@typescript-eslint/parser',
 	rules: {
+		// Prefers using type imports
 		'@typescript-eslint/consistent-type-imports': [
 			'warn',
 			{
@@ -15,12 +30,14 @@ module.exports = {
 			}
 		],
 		'no-redeclare': 0,
+		// Inline comment should be above
 		'line-comment-position': [
 			'warn',
 			{
 				position: 'above'
 			}
 		],
+		// Configure padding rules before and after variable declarations and statements
 		'padding-line-between-statements': [
 			'warn',
 			{
@@ -48,8 +65,11 @@ module.exports = {
 				next: ['if', 'do', 'while', 'for', 'switch', 'try', 'with']
 			}
 		],
+		// Disables the TypeScript specific no-redeclare rule
 		'@typescript-eslint/no-redeclare': 0,
+		// Warns if a button does not have an explicit type attribute
 		'react/button-has-type': 1,
+		// Named components must be arrow functions
 		'react/function-component-definition': [
 			1,
 			{
