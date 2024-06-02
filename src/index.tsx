09fc1cd448bc100e48922665f9d45d44ed98349d
Ilya Kruchinin <kr4chinin@mail.ru>
2024-06-02 18:56:09 +0300

    feat: error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import { App } from './App';
 
 export const queryClient = new QueryClient();
@@ -8,7 +9,9 @@ const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+  <ErrorBoundary fallback={<div>Something went wrong!</div>}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </ErrorBoundary>
 );
