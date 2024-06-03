8be57aae1a3a0f874791268fed6726d58f8280d9
Ilya Kruchinin <kr4chinin@mail.ru>
2024-06-03 18:25:08 +0300

    feat: createRouter, utilize in root index

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
-import { App } from './App';
+import { RouterProvider } from 'react-router-dom';
+import { createRouter } from './createRouter';
 
 export const queryClient = new QueryClient();
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
+const router = createRouter();
+
 root.render(
   <ErrorBoundary fallback={<div>Something went wrong!</div>}>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </ErrorBoundary>
 );
