5343fe4ebcd1f6ca445c48e0145c966aab3c97fe
Ilya Kruchinin <103210607+kr4chinin@users.noreply.github.com>
2024-06-03 18:26:58 +0300

    feat: react-router-dom integration (#1)
    * chore: add latest react-router-dom package
    
    * feat: createRouter, utilize in root index

---
+++ src/createRouter.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { App } from './App';
+
+export const createRouter = (): ReturnType<typeof createBrowserRouter> =>
+  createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+    },
+
+    // Wildcard route – this route will match all URLs that are not defined
+    {
+      path: '*',
+      element: <div>4️⃣0️⃣4️⃣ Page not found!</div>,
+    },
+  ]);
