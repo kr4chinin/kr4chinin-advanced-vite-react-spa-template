be8557d13da9adac5ced45c334a51ad438cec26b
Ilya Kruchinin <kr4chinin@mail.ru>
2024-06-03 19:39:00 +0300

    chore: import main.css into root index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import styled from 'styled-components';
 import ReactLogo from './assets/react.svg?react';
 
 const Root = styled.div`
+  height: 100dvh;
+  width: 100dvw;
+
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 100vh;
 `;
 
 const App = () => {
